3b03adddf0a655ca8db16eaeaca67482ef786fe8
K_ozgur <127993561+Dhr-Ozgur@users.noreply.github.com>
2025-10-21 21:18:55 +0200

    Delete src directory

--- src/core/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-ABUSEIPDB_KEY = os.getenv("ABUSEIPDB_KEY")
-VT_KEY = os.getenv("VT_KEY")
-RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")
-LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
